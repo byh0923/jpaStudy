9401c8192e656a44d734ba09a931593705d60426
YoungHyun Bae <byh2949@gmail.com>
2022-12-27 23:44:25 +0900

    영속성 관련 공부

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -16,20 +15,41 @@ public class JpaMain {
         tx.begin();
 
         try {
+            
+            /*// 비영속 상태
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
 
-            /*Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJPA");*/
+            // 영속 상태
+            System.out.println("===BEFORE===");
+            em.persist(member);
+            System.out.println("===AFTER===");*/
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
+            /*Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
 
-            for(Member member : result) {
-                System.out.println(member.getName());
+            // 쓰기 지연 SQL 저장소에 쌓아둠
+            em.persist(member1);
+            em.persist(member2);*/
 
-            }
+            // 플러시 관련
+            /*Member member = new Member(200L, "member200");
+            em.persist(member);
 
+            em.flush();*/
+
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAA");
+
+            // 준영속 상태 만드는 방법
+            /*em.detach(member);
+            em.clear();
+            em.close();*/
+            Member member2 = em.find(Member.class, 150L);
+
+
+            // 실제 쿼리 날라감
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,31 +1,23 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
+@Table(name="MEMBER")
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
 
     @Id
     private Long id;
-
     private String name;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
